d4c548d230b8a23f28d3775cb7479fa589425941
seantke <sean.tke@gmail.com>
2016-02-28 01:51:36 -0500

    too many things

--- public/controllers/loginController.js
+++ public/controllers/loginController.js
@@ -1,58 +1,62 @@
-angular.module("ToDo").controller("LoginController", function($scope, $http, $state){
-  // Variables
-  $scope.signUpInfo = {
-    username: undefined,
-    password:undefined,
-    repeatPassword:undefined
-  };
-  $scope.loginInfo = {
-    username: undefined,
-    password:undefined
-  };
-  // Functions
-
-  $scope.logUserIn = function(){
-    var data = {
-      username:$scope.loginInfo.username ,
-      password:$scope.loginInfo.password
-    }
-    $http.post("/login", data).success(function(response){
-      console.log(response);
-      localStorage.setItem("user", response);
-      $state.go("application");
-    }).error(function(error){
-      console.error(error);
-    });
-  }
-
-  $scope.logUserOut = function(){
-    localStorage.clear();
-    $state.go("login");
-  }
-
-  $scope.signUserUp = function(){
-    var data = {
-      username:$scope.signUpInfo.username ,
-      password:$scope.signUpInfo.password
+(function(){
+  angular.module("ToDo").controller("LoginController", function($scope, $rootScope, $http, $state){
+    // Variables
+    $scope.signUpInfo = {
+      username: undefined,
+      password:undefined,
+      repeatPassword:undefined
+    };
+    $scope.loginInfo = {
+      username: undefined,
+      password:undefined
+    };
+    $scope.loginErrors = { };
+    // Functions
+    $scope.logUserIn = function(refData){
+      var data = {
+        username:$scope.loginInfo.username || refData.username,
+        password:$scope.loginInfo.password || refData.password
+      }
+      $http.post("/login", data).success(function(response){
+        // console.log(response);
+        localStorage.setItem("user", response);
+        $rootScope.username = localStorage.user;
+        $state.go("application");
+      }).error(function(error){
+        $scope.loginErrors.logIn = "Username or Password Incorrect";
+        $scope.loginInfo = {};
+      });
     }
 
-    $http.post("/signup", data).success(function(response){
-      console.log(response);
-    }).error(function(error){
-      console.error(error);
-    });
-  };
-
-  $scope.matchPassword = function(){
-    if($scope.signUpInfo.password === $scope.signUpInfo.repeatPassword){
-      $scope.signUserUp();
-      $scope.signUpInfo = {};
-
-      return true;
+    $scope.logUserOut = function(){
+      localStorage.clear();
+      $state.go("login");
     }
-    else{
-      console.log("Passwords dont match");
-      return false;
-    }
-  };
-});
+
+    $scope.signUserUp = function(){
+      var data = {
+        username:$scope.signUpInfo.username ,
+        password:$scope.signUpInfo.password
+      }
+
+      $http.post("/signup", data).success(function(response){
+        // console.log(response);
+
+      }).error(function(error){
+        console.error(error);
+      });
+    };
+
+    $scope.matchPassword = function(){
+      if($scope.signUpInfo.password === $scope.signUpInfo.repeatPassword){
+        $scope.signUserUp();
+        // $scope.signUpInfo = {};
+        // return true;
+      }
+      else{
+        $scope.loginErrors.register = "Passwords don't match";
+        return false;
+      }
+    };
+  });
+}());
--- public/controllers/todoController.js
+++ public/controllers/todoController.js
@@ -1,47 +1,60 @@
-angular.module("ToDo").controller('todoController', ['$scope','$http','$state', function($scope, $http, $state){
-  if(!localStorage['user']){
-    $state.go('login');
-  }
-  else{
-    $state.go('application');
-  }
+(function(){
+  angular.module("ToDo").controller('todoController', ['$scope','$rootScope','$http','$state', function($scope, $rootScope, $http, $state){
+    if(!localStorage['user']){
+      $state.go('login');
+    }
+    else{
+      $rootScope.username = localStorage['user'];
+      $state.go('application');
+    }
 
-  $scope.todos = [];
+    $scope.todos = [];
 
-  $scope.getTodos = function(){
-    $http.get("/todos").success(function(data){
-      //console.log(data);
-      $scope.todos = [];
-      for(index in data){
-        $scope.todos.push({
-          'title': data[index].title,
-          'done':data[index].done
+    $scope.getTodos = function(){
+      var query = {
+        createdBy: $rootScope.username
+      };
+      $http.post("/todos/get", query).success(function(data){
+        //console.log(data);
+        $scope.todos = [];
+        for(index in data){
+          $scope.todos.push({
+            'title': data[index].title,
+            'createdBy': data[index].createdBy,
+            'done':data[index].done
+          });
+        }
+      }).error(function(error){
+        console.error(error);
+      });
+    };
+
+    $scope.addTodo = function(){
+      if($rootScope.username){
+        var newTodo = {
+          title:$scope.newTodo,
+          createdBy:$rootScope.username,
+          done:false
+        }
+        $http.post("/todos/create", newTodo).success(function(response){
+          //console.log(response);
+          $scope.newTodo = "";
+          $scope.getTodos();
+        }).error(function(error){
+          console.error(error);
         });
       }
-    }).error(function(error){
-      console.error(error);
-    });
-  };
+      else{
+        console.error("not signed in");
+      }
+    };
 
-  $scope.addTodo = function(){
-    var newTodo = {
-      title:$scope.newTodo,
-      done:false
-    }
-    $http.post("/todos", newTodo).success(function(response){
-      //console.log(response);
-      $scope.newTodo = "";
-      $scope.getTodos();
-    }).error(function(error){
-      console.error(error);
-    });
-  };
+    $scope.clearCompleted = function(){
+      $scope.todos = $scope.todos.filter(function(item){
+        return !item.done;
+      });
+    };
 
-  $scope.clearCompleted = function(){
-    $scope.todos = $scope.todos.filter(function(item){
-      return !item.done;
-    });
-  };
-
-  $scope.getTodos();
-}]);
+    $scope.getTodos();
+  }]);
+}());
